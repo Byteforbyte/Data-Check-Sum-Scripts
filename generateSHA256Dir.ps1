cc841af035f51807dc207a123604836d42a2b22a
Paul <109928755+Byteforbyte@users.noreply.github.com>
2022-08-01 18:02:00 -0400

    Update generateSHA256Dir.ps1

--- generateSHA256Dir.ps1
+++ generateSHA256Dir.ps1
@@ -1,9 +1,11 @@
+<# Check https://byteforbyte.io/tools for more information#>
+
 $dir = "C:\path\to\directory"
 $alldata = @()
 $files = Get-ChildItem -Path $dir* -Include *.* -Recurse | select target
 foreach($file in $files){
-$data = get-filehash $file.Target
-$alldata+=$data
+  $data = get-filehash $file.Target
+  $alldata+=$data
 }
 $date = get-date -f MM-dd-YYYY
 $exportpath = $dir + "\" + $date + "_checksum.csv"
